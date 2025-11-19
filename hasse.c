b630e98cffab61be3e0455c06d1c5b6e518865fc
Nathan <nathan.privey@efrei.net>
2025-11-19 17:55:09 +0100

    ceci est un petit commentaire

--- hasse.c
+++ hasse.c
@@ -1,49 +1,95 @@
 #include <malloc.h>
 #include "hasse.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
-void removeTransitiveLinks(t_link_array *p_link_array)
-{
-    int i = 0;
-    while (i < p_link_array->log_size)
-    {
-        t_link link1 = p_link_array->links[i];
-        int j = 0;
-        int to_remove = 0;
-        while (j < p_link_array->log_size && !to_remove)
-        {
-            if (j != i)
-            {
-                t_link link2 = p_link_array->links[j];
-                if (link1.from == link2.from)
-                {
-                    // look for a link from link2.to to link1.to
-                    int k = 0;
-                    while (k < p_link_array->log_size && !to_remove)
-                    {
-                        if (k != j && k != i)
-                        {
-                            t_link link3 = p_link_array->links[k];
-                            if ((link3.from == link2.to) && (link3.to == link1.to))
-                            {
-                                to_remove = 1;
-                            }
-                        }
-                        k++;
-                    }
-                }
-            }
-            j++;
-        }
-        if (to_remove)
-        {
-            // remove link1 by replacing it with the last link
-            p_link_array->links[i] = p_link_array->links[p_link_array->log_size - 1];
-            p_link_array->log_size--;
-        }
-        else
-        {
-            i++;
+t_link_array creer_reseau_hasse(ListeAdjacence *g, t_partition *p) {
+    t_link_array res;
+    res.links = NULL;
+    res.log_size = 0;
+
+    int n = g->taille;
+    int *map = (int*)malloc((n + 1) * sizeof(int));
+
+    if (!map) return res;
+
+    int i, j;
+    for (i = 0; i < p->nb_classes; i++) {
+        for (j = 0; j < p->classes[i].nb_sommets; j++) {
+            map[p->classes[i].sommets[j]] = i;
         }
     }
+
+    for (i = 1; i <= n; i++) {
+        Cellule *c = g->listes[i]->head;
+        while (c) {
+            int v = c->sommet_arrivee;
+            int c1 = map[i];
+            int c2 = map[v];
+
+            if (c1 != c2) {
+                int k;
+                int existe = 0;
+                for (k = 0; k < res.log_size; k++) {
+                    if (res.links[k].from == c1 && res.links[k].to == c2) {
+                        existe = 1;
+                    }
+                }
+
+                if (!existe) {
+                    res.log_size++;
+                    res.links = (t_link*)realloc(res.links, res.log_size * sizeof(t_link));
+                    res.links[res.log_size - 1].from = c1;
+                    res.links[res.log_size - 1].to = c2;
+                }
+            }
+            c = c->suivante;
+        }
+    }
+    free(map);
+
+    removeTransitiveLinks(&res);
+    return res;
+}
+
+void afficher_hasse(t_link_array *r) {
+    printf("\n--- Diagramme de Hasse ---\n");
+    if (r->log_size == 0) printf("Aucun lien.\n");
+
+    int i;
+    for (i = 0; i < r->log_size; i++) {
+        printf("C%d -> C%d\n", r->links[i].from + 1, r->links[i].to + 1);
+    }
+}
+
+void afficher_caracteristiques(t_partition *p, t_link_array *r) {
+    printf("\n--- Caracteristiques ---\n");
+
+    if (p->nb_classes == 1) printf("Graphe IRREDUCTIBLE.\n");
+    else printf("Graphe NON irreductible.\n");
+
+    int *transitoire = (int*)malloc(p->nb_classes * sizeof(int));
+    int i, k;
+
+    for (i = 0; i < p->nb_classes; i++) transitoire[i] = 0;
+
+
+    for (k = 0; k < r->log_size; k++) {
+        transitoire[r->links[k].from] = 1;
+    }
+
+    for (i = 0; i < p->nb_classes; i++) {
+        printf("C%d : ", i + 1);
+        if (transitoire[i]) {
+            printf("TRANSITOIRE\n");
+        } else {
+            printf("PERSISTANTE");
+            if (p->classes[i].nb_sommets == 1) {
+                printf(" (Etat ABSORBANT : %d)", p->classes[i].sommets[0]);
+            }
+            printf("\n");
+        }
+    }
+    free(transitoire);
 }
--- hasse.h
+++ hasse.h
@@ -1,14 +1,25 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
+#include "tarjan.h"
+#include "graph.h"
+
+typedef struct {
+    int from;
+    int to;
+} t_link;
+
+typedef struct {
+    t_link *links;
+    int log_size;
+} t_link_array;
+
+
 void removeTransitiveLinks(t_link_array *p_link_array);
 
-/**
- * @brief Creates a link array from the given partition and graph.
- *
- * @param part The partition of the graph.
- * @param graph The adjacency list representation of the graph.
- * @return The created link array.
- */
+
+t_link_array creer_reseau_hasse(ListeAdjacence *g, t_partition *p);
+void afficher_hasse(t_link_array *r);
+void afficher_caracteristiques(t_partition *p, t_link_array *r);
 
 #endif
